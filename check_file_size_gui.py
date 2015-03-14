c9c0f36937a0cb0d3dcdf5167aaacc79bb582f29
Akihiko Tanikawa <pureodio1109@gmail.com>
2015-03-14 23:08:54 +0900

    Change result_text from text area to grid.
    Grid method referred from http://stackoverflow.com/questions/28509629/work-with-ctrl-c-and-ctrl-v-to-copy-and-paste-into-a-wx-grid-in-wxpython .

--- check_file_size_gui.py
+++ check_file_size_gui.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import wx
+import wx.grid as gridlib
 import os
 import threading
 
@@ -43,6 +44,120 @@ class MyThreadKanshi(threading.Thread):
             self.window.file_list.SetItems(root_list)
             self.window.status.SetValue(u"未処理")
             self.window.drop_ctrl_event.clear()
+
+# http://stackoverflow.com/questions/28509629/work-with-ctrl-c-and-ctrl-v-to-copy-and-paste-into-a-wx-grid-in-wxpython
+class MyGrid(wx.grid.Grid):
+    """ A Copy&Paste enabled grid class"""
+    def __init__(self, parent, id, style):
+        wx.grid.Grid.__init__(self, parent, id, wx.DefaultPosition, wx.DefaultSize, style)
+        wx.EVT_KEY_DOWN(self, self.OnKey)
+        self.data4undo = [0, 0, '']
+
+    def OnKey(self, event):
+        # If Ctrl+C is pressed...
+        if event.ControlDown() and event.GetKeyCode() == 67:
+            self.copy()
+        # If Ctrl+V is pressed...
+        if event.ControlDown() and event.GetKeyCode() == 86:
+            self.paste('clip')
+        # If Ctrl+Z is pressed...
+        if event.ControlDown() and event.GetKeyCode() == 90:
+            if self.data4undo[2] != '':
+                self.paste('undo')
+        # If del is pressed...
+        if event.GetKeyCode() == 127:
+            # Call delete method
+            self.delete()
+        # Skip other Key events
+        if event.GetKeyCode():
+            event.Skip()
+            return
+
+    def copy(self):
+        # Number of rows and cols
+        if self.GetSelectionBlockTopLeft() == []:
+            rows = 1
+            cols = 1
+            iscell = True
+        else:
+            rows = self.GetSelectionBlockBottomRight()[0][0] - self.GetSelectionBlockTopLeft()[0][0] + 1
+            cols = self.GetSelectionBlockBottomRight()[0][1] - self.GetSelectionBlockTopLeft()[0][1] + 1
+            iscell = False
+        # data variable contain text that must be set in the clipboard
+        data = ''
+        # For each cell in selected range append the cell value in the data variable
+        # Tabs '\t' for cols and '\r' for rows
+        for r in range(rows):
+            for c in range(cols):
+                if iscell:
+                    data += str(self.GetCellValue(self.GetGridCursorRow() + r, self.GetGridCursorCol() + c))
+                else:
+                    data += str(self.GetCellValue(self.GetSelectionBlockTopLeft()[0][0] + r, self.GetSelectionBlockTopLeft()[0][1] + c))
+                if c < cols - 1:
+                    data += '\t'
+            data += '\n'
+        # Create text data object
+        clipboard = wx.TextDataObject()
+        # Set data object value
+        clipboard.SetText(data)
+        # Put the data in the clipboard
+        if wx.TheClipboard.Open():
+            wx.TheClipboard.SetData(clipboard)
+            wx.TheClipboard.Close()
+        else:
+            wx.MessageBox("Can't open the clipboard", "Error")
+
+    def paste(self, stage):
+        if stage == 'clip':
+            clipboard = wx.TextDataObject()
+            if wx.TheClipboard.Open():
+                wx.TheClipboard.GetData(clipboard)
+                wx.TheClipboard.Close()
+            else:
+                wx.MessageBox("Can't open the clipboard", "Error")
+            data = clipboard.GetText()
+            if self.GetSelectionBlockTopLeft() == []:
+                rowstart = self.GetGridCursorRow()
+                colstart = self.GetGridCursorCol()
+            else:
+                rowstart = self.GetSelectionBlockTopLeft()[0][0]
+                colstart = self.GetSelectionBlockTopLeft()[0][1]
+        elif stage == 'undo':
+            data = self.data4undo[2]
+            rowstart = self.data4undo[0]
+            colstart = self.data4undo[1]
+        else:
+            wx.MessageBox("Paste method "+stage+" does not exist", "Error")
+        text4undo = ''
+        # Convert text in a array of lines
+        for y, r in enumerate(data.splitlines()):
+            # Convert c in a array of text separated by tab
+            for x, c in enumerate(r.split('\t')):
+                if y + rowstart < self.NumberRows and x + colstart < self.NumberCols :
+                    text4undo += str(self.GetCellValue(rowstart + y, colstart + x)) + '\t'
+                    self.SetCellValue(rowstart + y, colstart + x, c)
+            text4undo = text4undo[:-1] + '\n'
+        if stage == 'clip':
+            self.data4undo = [rowstart, colstart, text4undo]
+        else:
+            self.data4undo = [0, 0, '']
+
+    def delete(self):
+        # print "Delete method"
+        # Number of rows and cols
+        if self.GetSelectionBlockTopLeft() == []:
+            rows = 1
+            cols = 1
+        else:
+            rows = self.GetSelectionBlockBottomRight()[0][0] - self.GetSelectionBlockTopLeft()[0][0] + 1
+            cols = self.GetSelectionBlockBottomRight()[0][1] - self.GetSelectionBlockTopLeft()[0][1] + 1
+        # Clear cells contents
+        for r in range(rows):
+            for c in range(cols):
+                if self.GetSelectionBlockTopLeft() == []:
+                    self.SetCellValue(self.GetGridCursorRow() + r, self.GetGridCursorCol() + c, '')
+                else:
+                    self.SetCellValue(self.GetSelectionBlockTopLeft()[0][0] + r, self.GetSelectionBlockTopLeft()[0][1] + c, '')
 
 class MyFileDropTarget(wx.FileDropTarget):
     def __init__(self, window):
@@ -87,7 +202,8 @@ class MyFrame(wx.Frame):
 
         # 3段目
         self.result_text_update_lock = threading.Lock()
-        self.result_text = wx.TextCtrl(root_pane, -1, "", style=wx.TE_MULTILINE)
+        self.result_text = MyGrid(root_pane, wx.ID_ANY, wx.ALL)
+        self.result_text.CreateGrid(0,2)
 
         # 縦組み
         layout.Add(sizer_1, 0, wx.ALL, 5)
@@ -105,10 +221,11 @@ class MyFrame(wx.Frame):
             for root, size_dir, skip_files in self.result_list[:]:
                 if root != selected:
                     continue
-                self.result_text.write(root + "\n")
                 for key in sorted(size_dir.keys()):
-                    self.result_text.write("\t".join([key, str(size_dir[key])]) + "\n")
-                self.result_text.write("\n")
+                    self.result_text.AppendRows()
+                    row = self.result_text.GetNumberRows() - 1
+                    self.result_text.SetCellValue(row, 0, key)
+                    self.result_text.SetCellValue(row, 1, str(size_dir[key]))
 
 if __name__ == '__main__':
     app = wx.PySimpleApp()
